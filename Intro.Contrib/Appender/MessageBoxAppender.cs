1815f97d35b4b1fc86f3197138f0de9705341c74
unknown <anthony.mastrean@.(none)>
2011-02-04 17:06:35 -0500

    added a custom error appender

---
+++ Intro.Contrib/Appender/MessageBoxAppender.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using log4net.Appender;
+using log4net.Core;
+
+namespace Intro.Contrib.Appender
+{
+    public class MessageBoxAppender : AppenderSkeleton
+    {
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            MessageBox.Show(loggingEvent.RenderedMessage, loggingEvent.Level.DisplayName);
+        }
+    }
+}
